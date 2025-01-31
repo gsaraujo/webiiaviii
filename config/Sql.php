27f0a87c0e7575709ce82d5624ad885915977066
Gustavo <sasaeucomvoce@hotmail.com>
2025-01-30 21:04:45 -0300

    fix: fixing Sql class

--- config/Sql.php
+++ config/Sql.php
@@ -1,18 +1,24 @@
 <?php
 
-
 class Sql
 {
-    public $hostname = "localhost";
+    public $hostname = "127.0.0.1";
     public $username = "root";
     public $password = "";
-    public $dbname = "iunidadewebi";
+    public $dbname = "ifbawebii";
 
-    protected $pdo;
-    public function __construct() {
+    protected $conn;
+
+    public function __construct()
+    {
+        $dsn = "mysql:host=" . $this->hostname . ";dbname=" . $this->dbname;
+
         try {
-
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
         }
+
 
     }
 
